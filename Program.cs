dfbaf86986234fb8a40eeccf76be66c6e2ff90e7
ClintonSBarnes <clintbarnes8@gmail.com>
2022-05-22 18:47:47 -0500

    C# Player's Guide - this is a simple menu choice using a SWITCHING expression.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,13 @@
 //create variables
 string response;
 int choice;
+string name;
+float cost;
+
+Console.WriteLine("Hello, friendly traveler. What is your name? ");
+name = Console.ReadLine();
+
+Console.WriteLine("Please look through our offerings.");
 
 //Generate the menu on the user's console
 Console.WriteLine("-------------------------------------------------------" +
@@ -22,16 +29,37 @@ Console.WriteLine("-------------------------------------------------------" +
 choice = Convert.ToInt16( Console.ReadLine());
 response = choice switch
 {
-    1 => "Rope costs 10 gold.",
-    2 => "Torches cost 15 gold.",
-    3 => "Climbing equipment costs 25 gold.",
-    4 => "Clean water costs 1 gold.",
-    5 => "Machetes cost 20 gold.",
-    6 => "Canoes cost 200 gold.",
-    7 => "Food supplies cost 1 gold.",
+    1 => "Rope",
+    2 => "Torches",
+    3 => "Climbing",
+    4 => "Clean water",
+    5 => "Machetes",
+    6 => "Canoes",
+    7 => "Food",
     _ => "Sorry, we don't carry any of those."
 };
 
-//output statement
-Console.WriteLine(response);
+cost = choice switch
+{
+    1 => 10,
+    2 => 15,
+    3 => 25,
+    4 => 1,
+    5 => 20,
+    6 => 200,
+    7 => 1,
+    _ => 0
+};
+
+//Determine if discount is applied and provide output regardless of customer name.
+if (name == "Clinton" ||name == "clinton")
+{
+    Console.WriteLine(name+", I see you are in the market for " + choice + ". Since you are such a good friend, I will only charge you " + cost / 2 + " gold.");
+}
+else
+{
+    Console.WriteLine(name + ", I see you are in the market for " + choice + ". That will be " + cost + " gold.");
+}
+
+//Hold from closing
 Console.ReadKey();
